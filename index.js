7607e2ade4536401323503bdd1d7bc33d8961c1c
Michell Ayala <michell.ayala@nuorder.com>
2020-04-13 00:06:07 -0500

    fix: export real from index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const point = require('./point')
 const set = require('./set')
 const number = require('./number')
+const real = require('./real')
 
 module.exports = {
   set,
   point,
-  number
+  number,
+  real
 }
