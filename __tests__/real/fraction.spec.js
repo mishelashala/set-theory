eedffa0c03dcbe32df646a4e6627b995a1100ccb
Michell Ayala <michell.ayala@nuorder.com>
2020-04-13 11:18:53 -0500

    update: test/real/fraction
    - add test case for both negative numbers (numerator, denominator)

--- __tests__/real/fraction.spec.js
+++ __tests__/real/fraction.spec.js
@@ -16,6 +16,11 @@ describe('freaction function', () => {
     expect(result).toEqual(['-', 1, 2])
   })
 
+  it('should create a fraction from -1 and -2', () => {
+    const result = fraction(-1, -2)
+    expect(result).toEqual(['+', 1, 2])
+  })
+
   it('should throw an exception if the denominator is 0', () => {
     try {
       fraction(-1, 0)
