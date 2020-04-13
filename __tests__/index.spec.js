a2bb2a9aecb5992a87262c8159229831e7cca258
Michell Ayala <michell.ayala@nuorder.com>
2020-04-12 22:20:54 -0500

    add: intersection implementation

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,4 +1,4 @@
-const { union } = require('../index')
+const { union, intersection } = require('../index')
 
 describe('set theory utils', () => {
   describe('union', () => {
@@ -22,6 +22,32 @@ describe('set theory utils', () => {
     it('should throw an error when a non-array is provided', () => {
       try {
         const result = union(null, null)
+        throw new Error('this test is broken')
+      } catch(err) {
+        expect(err.message).toBe('Illegal Argument: arguments must be arrays')
+      }
+    })
+  })
+
+  describe('intersection', () => {
+    it('should find the intersection of A and B given two non-empty arrays', () => {
+      const result = intersection([1, 2, 3], [3, 4, 5])
+      expect(result).toEqual([3])
+    })
+
+    it('should find the intersection of A and B given two empty arrays', () => {
+      const result = intersection([], [])
+      expect(result).toEqual([])
+    })
+
+    it('should find the intersection of A and B given undefined arguments', () => {
+      const result = intersection(undefined, undefined)
+      expect(result).toEqual([])
+    })
+
+    it('should throw an error when a non-array is provided', () => {
+      try {
+        const result = intersection(null, null)
         throw new Error('this test is broken')
       } catch(err) {
         expect(err.message).toBe('Illegal Argument: arguments must be arrays')
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Finds the mathematical union of two arrays
+ */
 function union(listA = [], listB = []) {
   if (!Array.isArray(listA) && !Array.isArray(listB)) {
     throw new Error('Illegal Argument: arguments must be arrays')
@@ -11,6 +14,29 @@ function union(listA = [], listB = []) {
   return Object.values(set)
 }
 
+/**
+ * Finds the mathematical intersection of two arrays
+ */
+function intersection(listA = [], listB = []) {
+  if (!Array.isArray(listA) && !Array.isArray(listB)) {
+    throw new Error('Illegal Argument: arguments must be arrays')
+  }
+
+  const combined = [...listA, ...listB]
+  const set = combined.reduce((acc, val, index) => {
+    const lastIndex = combined.lastIndexOf(val)
+
+    if (index !== lastIndex && !acc[val]) {
+      acc[val] = val
+    }
+
+    return acc
+  }, {})
+
+  return Object.values(set)
+}
+
 module.exports = {
-  union
+  union,
+  intersection
 }
